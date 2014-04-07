ca6b419fe52d2953d46ad44606e00450949cd8c0
Joao'S <Joao'S@10.3.2.255>
2014-04-07 17:19:23 -0300

    Modificar y agregar linea Eclipse

--- TallerBasicoGit/src/cl/taller/git/Main.java
+++ TallerBasicoGit/src/cl/taller/git/Main.java
@@ -4,7 +4,8 @@ public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("Mi segundo Commit");
-		System.out.println("Línea agregada desde la WEB")
+		System.out.println("Linea agregada desde la WEB, modificada desde Eclipse");
+		System.out.println("Linea desde Eclipse");
 
 	}
 
